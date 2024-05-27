550ca79075451408bb5e2c376fec01fe0a3e2db0
Korugan32 <tahacansenel@gmail.com>
2024-05-27 20:59:07 +0300

    Code Improvements

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
+import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         try {
             new LoginScreen(); // Giriş ekranını başlatır
         } catch (Exception e) {
-            e.printStackTrace(); // Hata durumunda hata mesajını yazdırır
+            JOptionPane.showMessageDialog(null,"Hata Mesajı: "+e);
         }
     }
 }
