dfb04137ba78e29de0fca975383b146b4ee56af5
Korugan32 <tahacansenel@gmail.com>
2024-05-27 21:15:42 +0300

    Fix Bugs, Add New Features

--- src/AppScreen.java
+++ src/AppScreen.java
@@ -11,6 +11,7 @@ public class AppScreen {
     private JTextField siteNameField, siteUsernameField;
     private JPasswordField sitePasswordField;
     private JButton editButton, saveButton;
+    private JProgressBar strengthBar;
 
     public AppScreen() {
         try {
@@ -42,21 +43,14 @@ public class AppScreen {
             leftButtonPanel.add(addNewPasswordButton);
             leftPanel.add(leftButtonPanel, BorderLayout.SOUTH);
 
-            // Parola oluşturma ekranını açar
-            passwordGeneratorButton.addActionListener(e -> new PasswordGenerator());
-            // Yeni parola ekleme ekranını açar
-            addNewPasswordButton.addActionListener(e -> {
-                PasswordStroge passwordStroge = new PasswordStroge();
-                passwordStorageList.add(passwordStroge);
-                passwordStroge.open();
-                updateSiteList();
-            });
-
-            JPanel rightPanel = new JPanel(new GridLayout(6, 1));
+            JPanel rightPanel = new JPanel(new GridLayout(8, 1));
 
             siteNameField = new JTextField();
             siteUsernameField = new JTextField();
             sitePasswordField = new JPasswordField();
+            siteNameField.setEditable(false);
+            sitePasswordField.setEditable(false);
+            siteUsernameField.setEditable(false);
             rightPanel.add(new JLabel("Site Name:"));
             rightPanel.add(siteNameField);
             rightPanel.add(new JLabel("Username:"));
@@ -66,20 +60,60 @@ public class AppScreen {
 
             editButton = new JButton("Edit");
             saveButton = new JButton("Save");
+            JCheckBox passView = new JCheckBox("Show Password");
+            JButton deleteButton = new JButton("Delete");
+            strengthBar = new JProgressBar();
+            strengthBar.setBorder(BorderFactory.createTitledBorder("Password Strength"));
             setButtonSize(editButton);
             setButtonSize(saveButton);
             editButton.setEnabled(false); // Başlangıçta düzenleme butonu devre dışı
             saveButton.setEnabled(false); // Başlangıçta kaydet butonu devre dışı
             rightPanel.add(editButton);
             rightPanel.add(saveButton);
+            rightPanel.add(passView);
+            rightPanel.add(deleteButton);
+            rightPanel.add(strengthBar);
 
-            siteList.addListSelectionListener(e -> updateRightPanel());
+            // Parola oluşturma ekranını açar
+            passwordGeneratorButton.addActionListener(e -> new PasswordGenerator());
+            // Yeni parola ekleme ekranını açar
+            addNewPasswordButton.addActionListener(e -> {
+                PasswordStroge passwordStroge = new PasswordStroge(siteListModel);
+                passwordStorageList.add(passwordStroge);
+                passwordStroge.open();
+            });
+
+            deleteButton.addActionListener(e -> {
+                if (siteList.getSelectedIndex()>-1){
+                    siteListModel.removeElementAt(siteList.getSelectedIndex());
+                    sitePasswordField.setText("");
+                    siteUsernameField.setText("");
+                    siteNameField.setText("");
+                    strengthBar.setValue(0);
+                }else {
+                    JOptionPane.showMessageDialog(frame,"Please Select Item");
+                }
+
+            });
+
+            passView.addActionListener(e -> {
+                if (passView.isSelected()) {
+                    sitePasswordField.setEchoChar((char) 0);
+                    sitePasswordField.putClientProperty("JPasswordField.cutCopyAllowed", true);
+                } else {
+                    sitePasswordField.setEchoChar('*');
+                }
+            });
+
+            siteList.addListSelectionListener(e -> {
+                updateRightPanel();
+            });
 
             // Düzenleme moduna geçer
             editButton.addActionListener(e -> {
                 editButton.setEnabled(false);
                 saveButton.setEnabled(true);
-                disableFields();
+                enableFields();
             });
 
             // Değişiklikleri kaydeder
@@ -91,10 +125,11 @@ public class AppScreen {
                         selectedStorage.setSiteName(siteNameField.getText());
                         selectedStorage.setUsername(siteUsernameField.getText());
                         selectedStorage.setPassword(new String(sitePasswordField.getPassword()));
+                        siteListModel.setElementAt(siteNameField.getText(),selectedIndex);
                         editButton.setEnabled(true);
                         saveButton.setEnabled(false);
-                        enableFields();
                         JOptionPane.showMessageDialog(frame, "Changes saved successfully.");
+                        disableFields();
                     }
                 } catch (Exception ex) {
                     ex.printStackTrace(); // Hata durumunda hata mesajını yazdırır
@@ -115,26 +150,18 @@ public class AppScreen {
         button.setPreferredSize(new Dimension(120, 30)); // Buton boyutunu ayarlar
     }
 
-    private void updateSiteList() {
-        siteListModel.clear();
-        for (PasswordStroge storage : passwordStorageList) {
-            siteListModel.addElement(storage.getSiteName()); // Site listesi güncellenir
-        }
-    }
-
     private void updateRightPanel() {
         try {
             int selectedIndex = siteList.getSelectedIndex();
             if (selectedIndex >= 0 && selectedIndex < passwordStorageList.size()) {
+                editButton.setEnabled(true);
                 PasswordStroge selectedStorage = passwordStorageList.get(selectedIndex);
+                PasswordStrength strength = new PasswordStrength(selectedStorage.getPassword());
                 siteNameField.setText(selectedStorage.getSiteName());
                 siteUsernameField.setText(selectedStorage.getUsername());
                 sitePasswordField.setText(selectedStorage.getPassword());
-                if (editButton.isEnabled()) {
-                    disableFields(); // Alanları düzenlenemez yapar
-                } else {
-                    enableFields(); // Alanları düzenlenebilir yapar
-                }
+                strengthBar.setValue(strength.calculateStrength());
+                strengthBar.setForeground(strength.calculateStrengthColor(strength.calculateStrength()));
             }
         } catch (Exception e) {
             e.printStackTrace(); // Hata durumunda hata mesajını yazdırır
